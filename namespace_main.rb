269f39fa58977bc7337de33700afdc32be8e129e
Jordan <jordan.baucke@gmail.com>
2011-12-14 11:19:42 -0700

    remove namespace.rb

--- namespace_main.rb
+++ namespace_main.rb
@@ -1,18 +1,32 @@
-custom_object_def_files = ['custom_objects_files.txt']
-
-custom_objects_fields = Array.new
-target_namespace = ''
-src_folder_path = ''
 
 
-target_namespace = 'NS__'
+class NamingConsideration <
+  # a Person has a first name, last name, and city
+  Struct.new(:unpackaged_name, :packaged_name)
+end
 
-custom_object_def_files.each{ |file|
-  #push all custom object names
-  File.open(file).each_line{ |line|
-    custom_objects_fields.push(line.chomp)
-    puts(line)
-  }
+src_folder_path = ARGV[0]
+
+#lines that need to be changed
+naming_considerations = Array.new
+File.open(ARGV[1]).each_line{ |line|
+	puts(line)
+	fields = line.split(':')
+	ns = NamingConsideration.new
+	ns.unpackaged_name = fields[0]
+	ns.packaged_name = fields[1].chomp #end of line
+	naming_considerations.push(ns)
+}
+
+#files that need to be renamed
+file_naming_considerations = Array.new
+File.open(ARGV[2]).each_line{ |line|
+	puts(line)
+	fields = line.split(':')
+	ns = NamingConsideration.new
+	ns.unpackaged_name = fields[0]
+	ns.packaged_name = fields[1].chomp #end of line
+	file_naming_considerations.push(ns)
 }
 
 #read in all the src files
@@ -23,31 +37,31 @@ src_folders = Dir.glob("**/");
 
 #go file by file
 src_folders.each{|folder|
+  changed = false
   Dir.chdir(src_folder_path)
   puts('Now entering: '+folder)
   Dir.chdir(folder);
   #get all source files in that directory
-  src_files = Dir.glob('*.{cls,page,object}')
+  src_files = Dir.glob('*.{cls,page,object,tab,app,layout}')
   src_files.each{ |src_file|
     puts('Now reading: '+src_file)
-    # fullpath = Dir.getwd+'/'+src_file
-    #search the file and replace
-    #file = File.open(fullpath)
-    text = File.read(src_file)
-    custom_objects_fields.each{ |customObjectFieldName|
-        text.gsub!(customObjectFieldName,target_namespace+customObjectFieldName)
-    }
-    
-    quick_fix = ['NS__NS__','NS__']
-    text.gsub!(quick_fix[0],quick_fix[1])
 
-    quick_fix = ['NS__ObjectNameNS__Status__c','NS__ObjectName__c']
-    text.gsub!(quick_fix[0],quick_fix[1])
+    text = File.read(src_file)
+
+    #loop over the naming considerations and apply it to each text file
+    naming_considerations.each{|namechange|
+      changed = true if text.gsub!(namechange.unpackaged_name,namechange.packaged_name)
+    }
 
     #write the updated text
     File.open(src_file,'w'){ |file|
-        file.puts text\
-    } if text != nil
+          file.puts text\
+    } if text != nil if changed
 
+
+    #loop over the file naming considerations attempt to rename
+    file_naming_considerations.each{|namechange|
+      File.rename(namechange.unpackaged_name,namechange.packaged_name) if src_file == namechange.unpackaged_name
+    }
   }
 }
